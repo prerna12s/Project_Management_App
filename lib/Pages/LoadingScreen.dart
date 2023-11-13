a1dd024aafcc6c162291005985431c07f98fecaf
prerna12s <tripathiprerna1203@gmail.com>
2023-11-13 23:11:01 +0530

    integrated firebase

---
+++ lib/Pages/LoadingScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Sign Up"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:project_management_/firebase_options.dart';
+import 'package:project_management_/Pages/LoadingScreen.dart';
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      debugShowCheckedModeBanner: false,
+      routes:{
+        '/':(context)=> LoadingPage(),
+      } ,
+    );
+  }}
